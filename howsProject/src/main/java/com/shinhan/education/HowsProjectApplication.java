94dbc40a08ba7e635f5830b1fb72b7f33c707a71
jinooyang <jinooyang@gmail.com>
2023-05-30 10:08:28 +0900

    first commit

---
+++ howsProject/src/main/java/com/shinhan/education/HowsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowsProjectApplication.class, args);
+	}
+
+}
---
+++ howsProject/src/test/java/com/shinhan/education/HowsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.education;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
